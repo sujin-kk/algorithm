876a2c8664d07cc7f381476cb0bce8d1f13d98cb
임수진 <sjoo0002@gmail.com>
2022-03-18 22:12:10 +0900

    백준 2438 별찍기

---
+++ backjoon/src/backjoon/b_2438.java
@@ -0,0 +1,18 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class b_2438 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        scan.close();
+
+        for (int i = 1; i <= N; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
